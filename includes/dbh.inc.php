ecfa2a62b46ddfb16d24891654aeb8a273edf816
joshdejeu <dejeujosh@gmail.com>
2025-04-04 08:41:19 -0500

    test env

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,16 +1,16 @@
 <?php
-
-$host = getenv("DATABASE_HOST");
-$dbname = getenv("DATABASE_NAME");
-
-$dsn = "mysql:host=$host;dbname=$dbname";
-$dbusername = getenv("DATABASE_USER");
-$dbpassword = getenv("DATABASE_PASSWORD");
+$url = parse_url(getenv("JAWSDB_URL"));
+$host = $url["host"];
+$dbname=substr($url["path"],1);
+$username = $url["user"];
+$password = $url["pass"];
+// $dbusername = getenv("DATABASE_USER");
+// $dbpassword = getenv("DATABASE_PASSWORD");
 
 // mysqli - mysql db
 // pdo - sqllite
 try {
-    $pdo = new PDO($dsn, $dbusername, $dbpassword); // attempt db connection
+    $pdo = new PDO($dsn, $username, $password); // attempt db connection
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // if error, throw exception
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
